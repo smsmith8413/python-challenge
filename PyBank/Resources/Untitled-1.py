aae135fb447e62a25966d789cc916e229c87b8ac
unknown <smsmith8413@gmail.com>
2019-12-22 02:31:05 -0600

    round 2!

--- PyBank/Resources/Untitled-1.py
+++
@@ -1,8 +0,0 @@
-import csv
-
-total = 0
-with open('budget_data.csv', newline='') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=' ',quotechar='|')
-    for row in spamreader:
-       total += int(row[1])
-
--- PyBank/main.py
+++ PyBank/main.py
@@ -1,32 +1,44 @@
-#bring in csv file
+# import CSV
 import os
 import csv
 
-from collections import Counter
-#read csv file
-budget_csv = os.path.join('python-challenge','PyBank','Resources','budget_data.csv')
-#open csv and specify delimiter
-#with open(budget_csv, newline='') as csvfile:
+#set patch to csv with relative ref
+budgetCsv = os.path.join('PyBank','Resources','budget_data.csv')
+
+# open csv as csvfile
+with open(budgetCsv, 'r', newline='') as csvfile:
+
     # CSV reader specifies delimiter and variable that holds contents
-#    csvreader = csv.reader(csvfile, delimiter=',')
-    # Read the header row first (skip this step if there is now header)
-    #csv_header = next(csvreader)
-    #print(f"CSV Header: {csv_header}")
-    # Loop through looking for the video
-    # Read each row of data after the header
-    ##data = list(csvreader)
-    ##date_count = len(data)
-    ##print(date_count) 
-#    dateCount = len(row[0])
-#    print(dateCount)
-# Test your function with the following:
-#print(length)
-#with open(udemy_csv, newline="") as csvfile:
-##csvreader = csv.reader(csvfile, delimiter=",")
-#def netPL
-#for row in csvreader:
-    # Determine percent of review left to 2 decimal places
-#    netPL = sum(float(row[2])
-with open(budget_csv) as csvfile:
-    csvfile.next()
-    total = sum(int(r[1]) for r in csv.reader(csvfile))
+    newBudget = csv.reader(csvfile, delimiter=',')
+    csv_header = next(newBudget)
+
+# declare variables (iterators)
+    totalMonths = []
+    netPL = []
+    monthlyChange = []
+    avgChange = []
+
+#loop through data in csv, appending along the way to be able to get # months and net P&L
+    for row in newBudget:
+        totalMonths.append(row[0])
+        netPL.append(float(row[1]))
+
+#loop through second 'column', calculating changes between rows and min/max values 
+    for i in range(1,len(netPL)):
+        monthlyChange.append(netPL[i] - netPL[i-1])
+        avgChange = sum(monthlyChange)/len(monthlyChange)
+        maxChangeDate = str(totalMonths[monthlyChange.index(max(monthlyChange))+1])
+        minChangeDate = str(totalMonths[monthlyChange.index(min(monthlyChange))+1])
+        maxChange = max(monthlyChange)
+        minChange = min(monthlyChange)
+
+#print results
+    print("Financial Analysis")
+    print("--------------------")
+    print("Total Months: ",len(totalMonths))
+    print("Total: $", round(sum(netPL)))
+    print("Average change: $",round(avgChange, 2))
+    print("Greatest increase in Profits: ",maxChangeDate, "($",round(maxChange),")")
+    print("Greatest decrease in Profits: ",minChangeDate,"($",round(minChange),")")
+
+
